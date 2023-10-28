109be44fda15a50def2fb9229c345bc69081963a
AbuBokorprog <abubokor1066@gmail.com>
2023-10-28 20:31:43 +0600

    theme set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/** @type {import('tailwindcss').Config} */
+//  @type {import('tailwindcss').Config}
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        50: "#eff9ff",
+        100: "#dff1ff",
+        200: "#b8e6ff",
+        300: "#78d2ff",
+        400: "#1fb6ff",
+        500: "#06a4f1",
+        600: "#0082ce",
+        700: "#0068a7",
+        800: "#02578a",
+        900: "#084972",
+        950: "#062e4b",
+      },
+    },
   },
   plugins: [],
 };
