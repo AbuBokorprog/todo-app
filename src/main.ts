3b0ac0cb49066f482e2f9fff40bc7c9369b014c1
AbuBokorprog <abubokor1066@gmail.com>
2023-10-28 22:15:24 +0600

    form setup

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,57 @@
 import "./style.css";
+
+// const form = document.getElementById("form") as HTMLFormElement;
+// const input = document.getElementById("taskInput") as HTMLInputElement;
+// // task container/ output
+// const container = document.getElementById("taskContainer") as HTMLDivElement;
+
+// type TODO = {
+//   task: string;
+//   isComplete: boolean;
+//   id: string;
+// };
+
+// const todos: Array<TODO> = [];
+
+// form.onsubmit = (e) => {
+//   e.preventDefault();
+//   //   const task = input.value;
+//   //   console.log(task);
+//   const todo: TODO = {
+//     task: input.value,
+//     isComplete: false,
+//     id: String(Math.random() * 1000),
+//   };
+//   todos.push(todo);
+//   renderTask();
+// };
+
+// const create = (task: string, isComplete: boolean, id: string) => {
+//   console.log(task, isComplete, id);
+//   //   container.innerHTML = `
+//   //     <div class="flex justify-between items-center gap-4">
+//   //     <input type="checkbox" name="" >
+//   //     <p>${task}</p>
+//   //     <button>X</button>
+//   //     </div>
+//   //     `;
+
+//   const div: HTMLDivElement = document.createElement("div");
+//   div.className = "flex justify-between items-center gap-4";
+//   const paragraph: HTMLParagraphElement = document.createElement("p");
+//   paragraph.innerText = `${task}`;
+//   const checkbox: HTMLInputElement = document.createElement("input");
+//   checkbox.setAttribute("type", "checkbox");
+//   checkbox.className = "checkbox checkbox-secondary";
+//   const delBtn: HTMLButtonElement = document.createElement("button");
+//   delBtn.innerText = "X";
+//   delBtn.className = "btn btn-circle btn-error";
+//   div.append(checkbox, paragraph, delBtn);
+//   container.append(div);
+// };
+
+// const renderTask = () => {
+//   todos.forEach((item) => {
+//     create(item.task, item.isComplete, item.id);
+//   });
+// };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,6 @@
 //  @type {import('tailwindcss').Config}
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    colors: {
-      primary: {
-        50: "#eff9ff",
-        100: "#dff1ff",
-        200: "#b8e6ff",
-        300: "#78d2ff",
-        400: "#1fb6ff",
-        500: "#06a4f1",
-        600: "#0082ce",
-        700: "#0068a7",
-        800: "#02578a",
-        900: "#084972",
-        950: "#062e4b",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  plugins: [require("daisyui")],
 };
