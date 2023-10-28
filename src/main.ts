9a509c3f578897a3860f89be51a4e1f2a8364609
AbuBokorprog <abubokor1066@gmail.com>
2023-10-28 22:17:52 +0600

    onsubmit implement on form

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,30 @@
 import "./style.css";
 
-// const form = document.getElementById("form") as HTMLFormElement;
-// const input = document.getElementById("taskInput") as HTMLInputElement;
-// // task container/ output
-// const container = document.getElementById("taskContainer") as HTMLDivElement;
+const form = document.getElementById("form") as HTMLFormElement;
+const input = document.getElementById("taskInput") as HTMLInputElement;
+// task container/ output
+const container = document.getElementById("taskContainer") as HTMLDivElement;
 
-// type TODO = {
-//   task: string;
-//   isComplete: boolean;
-//   id: string;
-// };
+type TODO = {
+  task: string;
+  isComplete: boolean;
+  id: string;
+};
 
-// const todos: Array<TODO> = [];
+const todos: Array<TODO> = [];
 
-// form.onsubmit = (e) => {
-//   e.preventDefault();
-//   //   const task = input.value;
-//   //   console.log(task);
-//   const todo: TODO = {
-//     task: input.value,
-//     isComplete: false,
-//     id: String(Math.random() * 1000),
-//   };
-//   todos.push(todo);
-//   renderTask();
-// };
+form.onsubmit = (e) => {
+  e.preventDefault();
+  //   const task = input.value;
+  //   console.log(task);
+  const todo: TODO = {
+    task: input.value,
+    isComplete: false,
+    id: String(Math.random() * 1000),
+  };
+  todos.push(todo);
+  //   renderTask();
+};
 
 // const create = (task: string, isComplete: boolean, id: string) => {
 //   console.log(task, isComplete, id);
